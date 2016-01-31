0fffe23adc4950362d1e0d575061544b21589a1b
abai <anxin.bai@gmail.com>
2016-01-31 16:25:49 -0500

    adding files from the teacher

---
+++ time.c
@@ -0,0 +1,26 @@
+//
+// time library
+//
+
+#include "time.h"
+
+
+
+unsigned int
+gettime( )
+{
+	// your code goes here
+}
+
+unsigned int
+timediff( unsigned int now, unsigned int then )
+{
+	// your code goes here
+}
+
+void
+wait( unsigned int time )
+{
+	// your code goes here
+}
+
